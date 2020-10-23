cd3df82375d3ed233f5914c6e0ec155ec033a2df
LDF <735697219@qq.com>
2020-10-23 23:16:16 +0800

    格式

--- LeetCode刷题/Leet300.py
+++ LeetCode刷题/Leet300.py
@@ -2,29 +2,30 @@ class Solution:
     def lengthOfLIS(self, nums):
         if not nums:
             return 0
-        n=len(nums)
-        dp=[1 for i in range(n)]
+        n = len(nums)
+        dp = [1 for i in range(n)]
         for i in range(n):
             for j in range(i):
-                if nums[i]>nums[j]:
-                    dp[i]=max(dp[j]+1,dp[i])
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[j] + 1, dp[i])
         return max(dp)
+
     def tanxin(self, nums):
-        n=len(nums)
-        if n<2:
+        n = len(nums)
+        if n < 2:
             return n
-        tail=[nums[0]]
-        for i in range(1,n):
-            if nums[i]>tail[-1]:
+        tail = [nums[0]]
+        for i in range(1, n):
+            if nums[i] > tail[-1]:
                 tail.append(nums[i])
             # 如果新来的数比末尾的小，开始二分查找
-            left=0
-            right=len(tail)-1
-            while left<right:
-                mid=(left+right)>>1
-                if tail[mid]<nums[i]:
-                    left=mid+1
+            left = 0
+            right = len(tail) - 1
+            while left < right:
+                mid = (left + right) >> 1
+                if tail[mid] < nums[i]:
+                    left = mid + 1
                 else:
-                    right=mid
-            tail[left]=nums[i]
+                    right = mid
+            tail[left] = nums[i]
         return len(tail)
